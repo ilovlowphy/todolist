80c46104c232712cef3e9a9b9c6ae633448f8a52
ilovlowphy <ilovlowphy@gmail.com>
2020-09-16 22:02:03 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
 
+<script>
+  import Header from '@/components/layout/Header';
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
 <style>
+@import "./assets/css/base.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="Header">
+    <div id="banner">To Do List</div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  #banner {
+    color: red;
+    background: lightgray;
+    font-size: 2em;
+  }
+</style>
